78481c4d9ba70e5b7ea75135e692f824160c8bb3
ajpetrizza <ajpetrizza@fullsail.edu>
2021-07-01 16:11:34 -0400

    Switched to dev branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Yo what up Josh
+          on dev branch
         </a>
       </header>
     </div>
